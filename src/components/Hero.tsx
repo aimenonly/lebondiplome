5e230e1bd3ebfab376ae1bf9de3441b1987ea83f
Aimen <122035147+aimenonly@users.noreply.github.com>
2024-03-30 12:50:27 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
   Investissez dans le bon diplome. 
 </h1>
 <h2 className="my-4 drop-shadow-lg regular-40 text-gray-500 lg:regular-40">
-Trouvez les emplois les plus demandés en France par région et par domaine.
+Trouvez les diplomes les plus demandés en France par région et par secteur d'activité.
 </h2>
 <div className="py-6 pt-8 flex flex-row justify-center mx-auto">
 <div className="flex flex-col items-center gap-3 sm:flex-row">
